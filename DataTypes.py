c9c7a5a7fd189996d6b6cd8e7a2a25afd8e9b3c7
shreyaslatthe <shreyaslatthe@gmail.com>
2025-01-11 15:14:50 +0530

    11jan py 2

---
+++ DataTypes.py
@@ -0,0 +1,48 @@
+#No need to declare any type of variable.
+
+i=32
+print("The value of i:", i)
+
+i=32+11.2
+print("The value of i as float:",i)
+
+i="test"
+print("The value of i as string:",i)
+
+
+x=10
+y=x
+print(x)
+print(y)
+
+print (id(x)) # print the identity ID of x
+print (id(y)) # print the identity ID of y
+
+min_bal=100
+print(min_bal)
+
+number_of_months=12
+print(number_of_months)
+numberOfMonths=12
+print(numberOfMonths)
+
+miles=101.2
+name="Tom"
+print(miles, name)
+
+a=b=c=1
+print(a,b,c)
+
+a,b,c = 3,4,"Test"
+print (a,b,c)
+
+#String options
+
+s1="Hello World"
+
+print (s1)
+print(s1[0])
+print(s1[2:7])
+print(s1[2:])
+print(s1*2)
+print(s1+" "+"Python")
---
+++ Indentation.py
@@ -0,0 +1,10 @@
+#Indentation
+
+a=10
+b = a+5
+print (b+5)
+if b>11:
+    print("Pass")
+else:
+    print("Fail")
+
